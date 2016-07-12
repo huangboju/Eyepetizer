8136d4c53c11d080a789e68a17dd3571935ed63a
huangboju <529940945@qq.com>
2016-07-12 10:42:46 +0800

    整理代码

--- Eyepetizer/Eyepetizer/Models/ItemModel.swift
+++ Eyepetizer/Eyepetizer/Models/ItemModel.swift
@@ -27,13 +27,9 @@ struct ItemModel {
     /// 模糊背景
     var blurred = ""
     /// 副标题
-    var subTitle = "" {
+    var subTitle: String {
         get {
-            if let _ = category,let _ = duration {
-                return "#\(category!)  /  \(Int.durationToTimer(duration!))"
-            } else {
-                return ""
-            }
+          return "#\(category)  /  \(Int.durationToTimer(duration))"
         }
     }
     // 喜欢数
@@ -43,32 +39,31 @@ struct ItemModel {
     // 评论数
     var replyCount = 0
     
-    init(dict : NSDictionary?) {
+    init(dict: [String : AnyObject]?) {
         self.type = dict!["type"] as? String ?? ""
         let dataDict = dict!["data"] as? NSDictionary ?? dict
         if let data = dataDict {
-            self.image = data["image"] as? String ?? nil
-            //                    self.image = self.image?.stringByReplacingOccurrencesOfString("jpeg", withString: "png")
-            self.text = data["text"] as? String ?? nil
-            self.id = data["id"] as? Int ?? 0
-            self.title = data["title"] as? String ?? ""
-            self.description = data["description"] as? String ?? ""
-            self.category = data["category"] as? String ?? ""
-            self.duration = data["duration"] as? Int ?? 0
-            self.playUrl = data["playUrl"] as? String ?? ""
+            image = data["image"] as? String ?? nil
+            text = data["text"] as? String ?? nil
+            id = data["id"] as? Int ?? 0
+            title = data["title"] as? String ?? ""
+            description = data["description"] as? String ?? ""
+            category = data["category"] as? String ?? ""
+            duration = data["duration"] as? Int ?? 0
+            playUrl = data["playUrl"] as? String ?? ""
             
             // 图片
-            let cover = data["cover"] as? NSDictionary ?? nil
+            let cover = data["cover"] as? [String : AnyObject] ?? nil
             if let coverDic = cover {
-                self.feed = coverDic["feed"] as? String ?? ""
-                self.blurred = coverDic["blurred"] as? String ?? ""
+                feed = coverDic["feed"] as? String ?? ""
+                blurred = coverDic["blurred"] as? String ?? ""
             }
             // 评论喜欢分享数量
-            let consumptionDict = data["consumption"] as? NSDictionary
+            let consumptionDict = data["consumption"] as? [String : AnyObject]
             if let consumption = consumptionDict {
-                self.collectionCount = consumption["collectionCount"] as? Int ?? 0
-                self.shareCount = consumption["shareCount"] as? Int ?? 0
-                self.replyCount = consumption["replyCount"] as? Int ?? 0
+                collectionCount = consumption["collectionCount"] as? Int ?? 0
+                shareCount = consumption["shareCount"] as? Int ?? 0
+                replyCount = consumption["replyCount"] as? Int ?? 0
             }
             
         }
@@ -89,51 +84,52 @@ struct IssueModel {
     var headerTitle: String?
     var headerImage: String?
     
-    var itemList : [ItemModel] = [ItemModel]()
+    var itemList = [ItemModel]()
     
-    init(dict : NSDictionary) {
-        self.date = dict["date"] as? Int16 ?? 0
-        self.publishTime = dict["publishTime"] as? Int16 ?? 0
-        self.type = dict["type"] as? String ?? ""
-        self.cound = dict["cound"] as? Int ?? 0
-        
-        let itemArray = dict["itemList"] as! [NSDictionary]
-        self.itemList = itemArray.map({ (dict) -> ItemModel in
-            return ItemModel(dict: dict)
-        })
+    init(dict: [String : AnyObject]) {
+        date = dict["date"] as? Int16 ?? 0
+        publishTime = dict["publishTime"] as? Int16 ?? 0
+        type = dict["type"] as? String ?? ""
+        cound = dict["cound"] as? Int ?? 0
+        if let itemArray = dict["itemList"] as? [[String : AnyObject]] {
+            self.itemList = itemArray.map({ (dict) -> ItemModel in
+                return ItemModel(dict: dict)
+            })
+        }
         
         // 判断是否有headerview
+        
         let firstItemModel = itemList.first
         if firstItemModel?.type == "video" {
-            self.isHaveSectionView = false
+            isHaveSectionView = false
         } else if firstItemModel?.type == "textHeader" {
-            self.isHaveSectionView = true
-            self.itemList.removeFirst()
-            self.headerTitle = firstItemModel?.text
+            isHaveSectionView = true
+            itemList.removeFirst()
+            headerTitle = firstItemModel?.text
         } else if firstItemModel?.type == "imageHeader" {
-            self.isHaveSectionView = true
-            self.itemList.removeFirst()
-            self.headerImage = firstItemModel?.image
+            isHaveSectionView = true
+            itemList.removeFirst()
+            headerImage = firstItemModel?.image
         } else {
-            self.isHaveSectionView = false
+            isHaveSectionView = false
         }
     }
 }
 
 struct EYEChoiceModel {
-    var issueList : [IssueModel] = [IssueModel]()
+    var issueList = [IssueModel]()
     /// 下一个page的地址
     var nextPageUrl = ""
     /// 下次更新的时间
     var nextPublishTime : Int16!
     var newestIssueType = ""
     
-    init(dict : NSDictionary) {
+    init(dict: [String : AnyObject]) {
         self.nextPageUrl = dict["nextPageUrl"] as? String ?? ""
         self.nextPublishTime = dict["nextPublishTime"] as? Int16 ?? 0
         self.newestIssueType = dict["newestIssueType"] as? String ?? ""
         
-        let issueArray = dict["issueList"] as! [NSDictionary]
+        let issueArray = dict["issueList"] as! [[String : AnyObject]]
         self.issueList = issueArray.map({ (dict) -> IssueModel in
             return IssueModel(dict: dict)
         })
