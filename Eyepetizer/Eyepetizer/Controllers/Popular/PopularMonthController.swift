eb29edfe3b9fff6d8efe40799c33deef0a977012
huangboju <529940945@qq.com>
2016-07-15 15:05:46 +0800

    整理代码

--- Eyepetizer/Eyepetizer/Controllers/Popular/PopularMonthController.swift
+++ Eyepetizer/Eyepetizer/Controllers/Popular/PopularMonthController.swift
@@ -7,7 +7,7 @@ import Alamofire
 class PopularMonthController: UIViewController, LoadingPresenter {
     var loaderView: LoaderView?
     var models = [ItemModel]()
-    
+
     private lazy var collectionView: CollectionView = {
         let rect = CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT - TAB_BAR_HEIGHT - CHARTS_HEIGHT - TOP_BAR_HEIGHT)
         let collectionView = CollectionView(frame: rect, collectionViewLayout:CollectionLayout())
@@ -19,17 +19,16 @@ class PopularMonthController: UIViewController, LoadingPresenter {
         collectionView.dataSource = self
         return collectionView
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(collectionView)
         setupLoaderView()
-        
+
         getData()
     }
-    
+
     private func getData() {
-        
         setLoaderViewHidden(false)
         Alamofire.request(.GET, APIHeaper.API_Popular_Monthly).responseSwiftyJSON ({ [unowned self](request, response, json, error) in
             // 字典转模型 刷新数据
@@ -45,7 +44,7 @@ class PopularMonthController: UIViewController, LoadingPresenter {
             self.setLoaderViewHidden(true)
             })
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -55,17 +54,17 @@ extension PopularMonthController: UICollectionViewDelegate, UICollectionViewData
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return models.count
     }
-    
+
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         return collectionView.dequeueReusableCellWithReuseIdentifier(ChoiceCell.cellID, forIndexPath: indexPath)
     }
-    
+
     func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
         let cell = cell as? ChoiceCell
         cell?.model = models[indexPath.row]
         cell?.index = "\(indexPath.row + 1)"
     }
-    
+
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         if parentViewController is PopularController {
             (parentViewController as? PopularController)?.selectCell = collectionView.cellForItemAtIndexPath(indexPath) as? ChoiceCell
@@ -73,7 +72,7 @@ extension PopularMonthController: UICollectionViewDelegate, UICollectionViewData
         let model = models[indexPath.row]
         navigationController?.pushViewController(VideoDetailController(model: model), animated: true)
     }
-    
+
     func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
         if kind == UICollectionElementKindSectionFooter {
             let footView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: PopularFooterView.reuseIdentifier, forIndexPath: indexPath)
